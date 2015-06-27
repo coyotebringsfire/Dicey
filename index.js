4a4c5f4f51860775692d9f99c77e312e9f6a84e2
CoyoteBringsFire <aumkara@gmail.com>
2015-06-26 23:14:01 -0700

    remove sample code, unneeded session management

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var Die=require('./Die');
+
 /**
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
  * For additional samples, visit the Alexa Skills Kit developer documentation at
@@ -18,11 +20,7 @@ exports.handler = function (event, context) {
         if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
              context.fail("Invalid Application ID");
          }
-        */
-
-        if (event.session.new) {
-            onSessionStarted({requestId: event.request.requestId}, event.session);
-        }
+         */
 
         if (event.request.type === "LaunchRequest") {
             onLaunch(event.request,
@@ -46,14 +44,6 @@ exports.handler = function (event, context) {
 };
 
 /**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-                + ", sessionId=" + session.sessionId);
-}
-
-/**
  * Called when the user launches the skill without specifying what they want.
  */
 function onLaunch(launchRequest, session, callback) {
@@ -75,25 +65,11 @@ function onIntent(intentRequest, session, callback) {
         intentName = intentRequest.intent.name;
 
     // Dispatch to your skill's intent handlers
-    if ("MyColorIsIntent" === intentName) {
-        setColorInSession(intent, session, callback);
-    } else if ("WhatsMyColorIntent" === intentName) {
-        getColorFromSession(intent, session, callback);
-    } else if ("HelpIntent" === intentName) {
-        getWelcomeResponse(callback);
+    if ("Roll" === intentName) {
+        Roll(intent, session, callback);
     } else {
         throw "Invalid intent";
     }
-}
-
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-                + ", sessionId=" + session.sessionId);
-    // Add cleanup logic here
 }
 
 // --------------- Functions that control the skill's behavior -----------------------
@@ -102,14 +78,13 @@ function getWelcomeResponse(callback) {
     // If we wanted to initialize the session to have some attributes we could add those here.
     var sessionAttributes = {};
     var cardTitle = "Welcome";
-    var speechOutput = "Welcome to the Alexa Skills Kit sample, "
-                + "Please tell me your favorite color by saying, "
-                + "my favorite color is red";
+    var speechOutput = "Welcome to Dicey, "
+                + "Please tell me what you want rolled, ";
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
-    var repromptText = "Please tell me your favorite color by saying, "
-                + "my favorite color is red";
-    var shouldEndSession = false;
+    var repromptText = "Please tell me what you want rolled by saying, "
+                + "roll three d six minus one";
+    var shouldEndSession = true;
 
     callback(sessionAttributes,
              buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
@@ -118,63 +93,65 @@ function getWelcomeResponse(callback) {
 /**
  * Sets the color in the session and prepares the speech to reply to the user.
  */
-function setColorInSession(intent, session, callback) {
-    var cardTitle = intent.name;
-    var favoriteColorSlot = intent.slots.Color;
-    var repromptText = "";
-    var sessionAttributes = {};
-    var shouldEndSession = false;
-    var speechOutput = "";
+function Roll(intent, session, callback) {
+    var cardTitle = intent.name,
+        diceToRollSlot = intent.slots.Dice,
+        diceToRoll = "",
+        repromptText = "",
+        sessionAttributes = {},
+        shouldEndSession = true,
+        speechOutput = "";
 
-    if (favoriteColorSlot) {
-        favoriteColor = favoriteColorSlot.value;
+    if (diceToRollSlot) {
+        diceToRoll = diceToRollSlot.value;
+        // parse the request and roll dice
         sessionAttributes = createFavoriteColorAttributes(favoriteColor);
-        speechOutput = "I now know your favorite color is " + favoriteColor + ". You can ask me "
-                + "your favorite color by saying, what's my favorite color?";
-        repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
+        speechOutput = toWords(doRoll(diceToRoll));
+        repromptText = "You can ask me to roll dice by saying, roll three d six";
     } else {
-        speechOutput = "I'm not sure what your favorite color is, please try again";
-        repromptText = "I'm not sure what your favorite color is, you can tell me your "
-                + "favorite color by saying, my favorite color is red";
+        speechOutput = "I'm not sure what you want me to roll, please try again";
+        repromptText = "I'm not sure what you want me to roll, You can ask me "
+                + "to roll dice by saying, roll three d six";
     }
 
     callback(sessionAttributes,
              buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
 }
 
-function createFavoriteColorAttributes(favoriteColor) {
-    return {
-        favoriteColor: favoriteColor
-    };
+var doRoll=function(words) {
+    var operands=[], operator, d,
+        currentWord=words.shift();
+    while( currentWord ) {
+        switch(true) {
+            case currentWord.match(/[\d]+d[\d]+/)!=null:
+                d=new Die();
+                var rollResult=d.roll(currentWord);
+                operands.push( rollResult.sum );
+                if( operands.length > 1) {
+                    doOperator(operator, operands);
+                }
+                break;
+            case currentWord.match(/[\d]+/)!=null:
+                operands.push(parseInt(currentWord));
+                if( operands.length > 1) {
+                    doOperator(operator, operands);
+                }
+                break;
+            case currentWord==="+":
+            case currentWord==="plus":
+                operator="+";
+                break;
+            case currentWord==="-":
+            case currentWord==='minus':
+                operator="-";
+                break;
+        }
+        currentWord=words.shift();
+    }
+
+    return operands[0];
 }
 
-function getColorFromSession(intent, session, callback) {
-    var cardTitle = intent.name;
-    var favoriteColor;
-    var repromptText = null;
-    var sessionAttributes = {};
-    var shouldEndSession = false;
-    var speechOutput = "";
-
-    if(session.attributes) {
-        favoriteColor = session.attributes.favoriteColor;
-    }
-
-    if(favoriteColor) {
-        speechOutput = "Your favorite color is " + favoriteColor + ", goodbye";
-        shouldEndSession = true;
-    }
-    else {
-        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color "
-                + " is red";
-    }
-
-    // Setting repromptText to null signifies that we do not want to reprompt the user.
-    // If the user does not respond or says something that is not understood, the session
-    // will end.
-    callback(sessionAttributes,
-             buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
-}
 
 // --------------- Helpers that build all of the responses -----------------------
 
